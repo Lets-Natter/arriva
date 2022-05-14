707bb6cdf88470d640efd8cb3d558a6f1f0651bc
Qasim Abdullah <qasimabdullah404@gmail.com>
2022-05-14 17:19:25 +0500

    add localhost ip and make calls async

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:5000/events", event); // Post an event to natter service
-  axios.post("http://localhost:5001/events", event); // Post an event to cross-natter service
-  axios.post("http://localhost:5002/events", event); // Post an event to query engine service
+  await axios.post("http://localhost:5000/events", event); // Post an event to natter service
+  await axios.post("http://localhost:5001/events", event); // Post an event to cross-natter service
+  await axios.post("http://localhost:5002/events", event); // Post an event to query engine service
 
   res.send({ status: "OK" });
 });
 
-app.listen(5005, () => console.log("Listening on 5005"));
+app.listen(5005, "127.0.0.1", () => console.log("Listening on 5005"));
